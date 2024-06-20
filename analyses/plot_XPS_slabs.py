49f2da266f9fc8284cfd0be02f79c80c9513ef6d
Pierre Beaujean <pierreb24@gmail.com>
2024-06-20 13:33:05 +0200

    SJ vs SJn

--- analyses/plot_XPS_slabs.py
+++ analyses/plot_XPS_slabs.py
@@ -18,20 +18,24 @@ def plot_slab(ax, data: pandas.DataFrame, system: str, atom: str, ref: float, xr
         ax.plot(x, i + create_spectrum(subdata, x, ref), '-')
         ax.text(xrange[1] - .25, i + .25, subsystem * (4 if system == 'CaH2' else 2))
     
-    ax.text(xrange[0], 6.5, '{} / {}{}s'.format(system.replace('H2', 'H$_2$'), atom, 2 if atom == 'Ca' else 1), fontsize=12)
+    ax.text(xrange[1], 6.5, '{} / {}{}s'.format(system.replace('H2', 'H$_2$'), atom, 2 if atom == 'Ca' else 1), fontsize=12)
 
         
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input', default='../data/Data_XPS_slabs.csv')
+parser.add_argument('-i', '--input', default='../data/Data_XPS_slab_SJ.csv')
+parser.add_argument('-t', '--type', default='SJ')
 parser.add_argument('-o', '--output', default='Data_XPS_slabs.pdf')
 
 args = parser.parse_args()
 
 data = pandas.read_csv(args.input)
-# REF_CA = 426.53
-# REF_O = 546.08 # SJ
-REF_CA = 426.56
-REF_O = 548.79 # in SJn
+
+if args.type == 'SJn':
+    REF_CA = 426.56
+    REF_O = 548.79 # in SJn
+else:
+    REF_CA = 426.53
+    REF_O = 546.08 # SJ
 
 figure = plt.figure(figsize=(10, 7))
 (ax1, ax2), (ax3, ax4) = figure.subplots(2, 2, sharey=True)
@@ -39,8 +43,9 @@ figure = plt.figure(figsize=(10, 7))
 plot_slab(ax1, data, 'Ca', 'Ca', REF_CA, (-1.5, 1.5))
 plot_slab(ax2, data, 'CaH2', 'Ca', REF_CA, (-1.5, 1.5))
 plot_slab(ax3, data, 'CaO', 'Ca', REF_CA, (-1.5, 1.5))
-plot_slab(ax4, data, 'CaO', 'O', REF_O, (-9, -6))
+plot_slab(ax4, data, 'CaO', 'O', REF_O, (-9, -6) if args.type == 'SJn' else (-7, -4))
 
+[ax.invert_xaxis() for ax in [ax1, ax2, ax3, ax4]]
 [ax.set_xlabel('$\\Delta$BE (eV)') for ax in [ax3, ax4]]
 [ax.yaxis.set_major_formatter('') for ax in [ax1, ax3]]
 
