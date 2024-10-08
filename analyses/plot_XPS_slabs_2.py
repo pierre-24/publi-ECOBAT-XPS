0225999ff1bd2ff05cfb52d64e2d86d81bc63407
pierre-24 <pierreb24@gmail.com>
2024-10-08 20:51:27 +0200

    CaO in Fig. 7

--- analyses/plot_XPS_slabs_2.py
+++ analyses/plot_XPS_slabs_2.py
@@ -6,7 +6,7 @@ import argparse
 from XPS.commons import create_spectrum_BE
 
 
-def prepare_data(data: pandas.DataFrame, data_height: pandas.DataFrame):
+def prepare_data(data: pandas.DataFrame, data_height: pandas.DataFrame, systems: list):
     REFS = {  # references
         'Ca': data[(data['System'] == 'Ca_slab/8') & (data['Atom_indices' ].str.contains('Ca_037'))].iloc[0],
         'O':  data[(data['System'] == 'ref_O')].iloc[0]
@@ -17,21 +17,28 @@ def prepare_data(data: pandas.DataFrame, data_height: pandas.DataFrame):
     is_surf = []
     is_OH = []
     
-    data_out = data[data['System'] != 'ref_O']
+    data_out = data[data['System'].isin(systems)]
     
     for line in data_out.itertuples():
         atom = line.Atom
         delta_computed.append(line.Value - REFS[atom]['Value'])
+        has_OH2 = 'OH2' in line.System
         
         isb, iss, iso = False, False, False
         for a in line.Atom_indices.split(';'):
             h = data_height[(data_height['System'] == line.System) & (data_height['Atom' ] == a)].iloc[0]
-            if h['z_depth'] <= 0.35:
-                isb = True
-            if 0.35 < h['z_depth'] <= 0.4:
-                iss = True
-            if h['z_depth'] > 0.4:
-                iso = True
+            if has_OH2:
+                if h['z_depth'] <= 0.35:
+                    isb = True
+                if 0.35 < h['z_depth'] <= 0.4:
+                    iss = True
+                if h['z_depth'] > 0.4:
+                    iso = True
+            else:
+                if h['z_depth'] <= 0.4:
+                    isb = True
+                if h['z_depth'] > 0.4:
+                    iss = True 
                 
             if iss and isb and iso:
                 break
@@ -48,31 +55,28 @@ def prepare_data(data: pandas.DataFrame, data_height: pandas.DataFrame):
     return data_out
         
 
-def plot_atom(ax, data: pandas.DataFrame, system: str, atom: str, color: str, label: str, shift_y: float = .0, space: tuple = (-10, 10)):
+def plot_atom(ax, data: pandas.DataFrame, system: str, atom: str, color: str, linestyle: str, label: str, shift_y: float = .0, space: tuple = (-10, 10)):
     subdata = data[(data['System'] == system) & (data['Atom'] == atom)]
     
     lspace = numpy.linspace(*space, 200)
     y = create_spectrum_BE(subdata, lspace)
-    ax.plot(lspace, y + shift_y, label=label, color=color)
+    ax.plot(lspace, y + shift_y, label=label if linestyle == '-' else None, color=color, linestyle=linestyle)
     
     m_bulk = subdata[subdata['Is_bulk'] == True]['Delta_computed']
-    ax.plot([m_bulk.mean(), m_bulk.mean()], [shift_y, shift_y + 0.3], '-', color=color)
-    ax.text(m_bulk.mean(), shift_y + .3, 'b', va='bottom', ha='center', color=color)
+    ax.plot([m_bulk.mean(), m_bulk.mean()], [shift_y, shift_y + 0.3], color=color, linestyle=linestyle)
+    ax.text(m_bulk.mean(), shift_y + (.3 if linestyle == '-' else 0), 'b', va='bottom' if linestyle == '-' else 'top', ha='center', color=color)
     m_surf = subdata[subdata['Is_surf'] == True]['Delta_computed']
-    ax.plot([m_surf.mean(), m_surf.mean()], [shift_y, shift_y + 0.3], '-', color=color)
-    ax.text(m_surf.mean(), shift_y + .3, 's', va='bottom', ha='center', color=color)
+    ax.plot([m_surf.mean(), m_surf.mean()], [shift_y, shift_y + 0.3], color=color, linestyle=linestyle)
+    ax.text(m_surf.mean(), shift_y + (.3 if linestyle == '-' else 0), 's', va='bottom' if linestyle == '-' else 'top', ha='center', color=color)
     
-    print(label)
-    print('CaO_OH2,{},{:.2f} $\\pm$ {:.2f},{:.2f} $\\pm$ {:.2f}'.format(atom, m_bulk.mean(), m_bulk.std(), m_surf.mean(), m_surf.std()))
-    
-    if atom == 'O':
+    if atom == 'O' and system == 'CaO_OH2_slab/3':
         m_OH = subdata[subdata['Is_OH'] == True]['Delta_computed']
-        ax.plot([m_OH.mean(), m_OH.mean()], [shift_y, shift_y + 0.3], '-', color=color)
+        ax.plot([m_OH.mean(), m_OH.mean()], [shift_y, shift_y + 0.3], color=color, linestyle=linestyle)
         
 parser = argparse.ArgumentParser()
 parser.add_argument('inputs', nargs='*')
 parser.add_argument('--height', default='../data/results_slabs/slabs_height.csv')
-parser.add_argument('-s', '--system', default='CaO_OH2_slab/3')
+parser.add_argument('-s', '--systems', nargs='*', default=['CaO_OH2_slab/3', 'CaO_slab/3'])
 parser.add_argument('-a', '--atoms', nargs='*')
 parser.add_argument('-n', '--names', nargs='*', required=True)
 parser.add_argument('-o', '--output', default='Data_XPS_slabs.pdf')
@@ -87,22 +91,24 @@ data_height = pandas.read_csv(args.height)
 
 data = []
 for inp in args.inputs:
-    data.append(prepare_data(pandas.read_csv(inp), data_height))
+    data.append(prepare_data(pandas.read_csv(inp), data_height, args.systems))
 
 figure = plt.figure(figsize=(len(args.atoms) * 5, 6))
 axes = figure.subplots(1, len(args.atoms))
 
 COLORS = ['tab:blue', 'tab:pink', 'tab:green', 'tab:red', 'tab:cyan']
+LINESTYLES = ['-', '--']
 
 YS = 1.5
 
 for i, subdata in enumerate(data):
     for j, atom in enumerate(args.atoms):
         a, ami, amax = atom.split(':')
-        plot_atom(axes[j], subdata, args.system, a, COLORS[i], args.names[i], shift_y = i * YS, space = (float(ami), float(amax)))
+        for k, system in enumerate(args.systems):
+            plot_atom(axes[j], subdata, system, a, COLORS[i], LINESTYLES[k], args.names[i], shift_y = i * YS, space = (float(ami), float(amax)))
         
         if i == 0:
-            axes[j].text(.95, .95, 'CaO$\\cdot$H$_2$O / {} {}'.format(a, '2s' if a == 'Ca' else '1s'), transform=axes[j].transAxes, fontsize=14, ha='right')
+            axes[j].text(.95, .95, '{} {}'.format(a, '2s' if a == 'Ca' else '1s'), transform=axes[j].transAxes, fontsize=14, ha='right')
 
 [ax.legend(loc='upper left') for ax in axes]
 [ax.invert_xaxis() for ax in axes]
